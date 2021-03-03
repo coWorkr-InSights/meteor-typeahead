60dea0f266e22cfd9bc437e4e9dd3f1dac4d4329
Timothy Pfafman <tim@pfafman.com>
2021-03-03 07:42:44 -0700

    remove old deps

--- package.js
+++ package.js
@@ -2,12 +2,12 @@ Package.describe({
 	name: "sergeyt:typeahead",
 	summary: "Autocomplete package for meteor powered by twitter typeahead.js",
 	git: "https://github.com/sergeyt/meteor-typeahead.git",
-	version: "1.2.0"
+	version: "1.2.1"
 });
 
 Package.onUse(function(api) {
-	api.versionsFrom('METEOR@1.2');
-	api.use(['jquery', 'blaze@1.0.0 || 2.0.0', 'ui'], 'client');
+	//api.versionsFrom('METEOR@1.2');
+	api.use(['jquery', 'blaze', 'ui'], 'client');
 	api.use('twbs:bootstrap@3.0.0', 'client', {weak: true});
 	api.addFiles('typeahead.bundle.js', 'client');
 	api.addFiles('index.js', 'client');
